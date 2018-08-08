6572800f7f38bc0132ea03bad5f6e24d5e23e08c
wingy <liu.wingy@gmail.com>
2018-08-08 17:06:24 +0200

    mes foodfriend vuejs

--- myappWeb/src/main/webapp/js/foodfriendtestJ2.js
+++ myappWeb/src/main/webapp/js/foodfriendtestJ2.js
@@ -1,20 +1,3 @@
-/*export default {
-	data() {
-		return {
-			
-		}
-	},
-	methods: {
-		
-	},
-	created() {
-		this.$http.get('http://localhost:8080/myappWeb/services/rest/stock/1')
-		.then(function(data){
-			console.log(data);
-		})
-	}
-}*/
-
 var id = document.getElementById('iduser').value;
 
 var mesFoodfriend = new Vue({
@@ -22,27 +5,51 @@ var mesFoodfriend = new Vue({
   el: '#listeFoodfriend',
   data: {
     foodfriend: [],
-    titre:"Liste de mes foodfriend",
-    iduser: id
+    iduser: id,
+    src: "./resources/img/utilisateur/",
+    imgtype: ".jpeg"
+    	
   },
-  
-  
+  methods: {
+	getSrc: function(pseudo) {
+		return this.src + pseudo + this.imgtype;
+	}  
+  },
+   
   created: function() {
     var vm = this
     axios.get('http://localhost:8080/myappWeb/services/rest/foodfriend/mesfoodfriend?iduser=' + id)
       .then(function (response) {
         vm.foodfriend = response.data
       })
-  },
+  }
   
-  request: function() {
+})
+
+var mesDemandesFFRecues = new Vue({
+	  
+	  el: '#listeDemandesFoodfriend',
+	  data: {
+	    foodfriend: [],
+	    iduser: id,
+	    src: "./resources/img/utilisateur/",
+	    imgtype: ".jpeg"
+	    	
+	  },
+	  methods: {
+		getSrc: function(pseudo) {
+			return this.src + pseudo + this.imgtype;
+		}  
+	  },
+	   
+	  created: function() {
 	    var vm = this
 	    axios.get('http://localhost:8080/myappWeb/services/rest/foodfriend/mesdemandesrecues?iduser=' + id)
 	      .then(function (response) {
 	        vm.foodfriend = response.data
 	      })
 	  }
-  
-})
+	  
+	})
 
 
