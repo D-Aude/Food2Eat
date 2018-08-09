6f910f2f45a0de9eb15cc3fc3184b91343512119
FunkyJF <jeanfrancois.anthony@gmail.com>
2018-08-09 17:10:32 +0200

    bar progression+eventBouton

--- myappWeb/src/main/webapp/js/StockJS.js
+++ myappWeb/src/main/webapp/js/StockJS.js
@@ -1,69 +1,76 @@
-var filtres = new Vue({
-  el: '#filtres',
-  data: {
-	  	id: '',
-	  	modeConservation: '',
-	    stocks: []
-	  },
-  watch: {
-	  modeConservation: function (mode, oldMode) {
-		  var id = document.getElementById('idUtilisateur').value;
-		  console.log('Stock.parModeConservation: id='+id+', modeConservation='+mode);
-		  	var filtres = this
-		  	if(mode == "")
-		  	{
-		  		axios.get('http://localhost:8080/myappWeb/services/rest/stock?id='+id)
-			      .then(function (response) {
-			    	  filtres.stocks = response.data
-			      })
-		  	}
-		  	else
-		  	{
-		  		axios.get('http://localhost:8080/myappWeb/services/rest/stock?id='+id+'&mode='+mode)
-			      .then(function (response) {
-			    	  filtres.stocks = response.data
-			      })
-		  	}
-		    
-	  }
-  },
-  methods : {   
-    chargerStock: function ()
-	{
-    	var id = document.getElementById('idUtilisateur').value;
-		console.log('Stock.parUtilisateur: '+id);
-    	var filtres = this
-        axios.get('http://localhost:8080/myappWeb/services/rest/stock/'+id)
-          .then(function (response) {
-        	  filtres.stocks = response.data
-          })
-	}
-  }
-})
+var id = document.getElementById('idUtilisateur').value;
 
 var vm = new Vue({
-  el: '#app',
+  el: '#gestionStock',
   data: {
-    stocks: []
+    stocks: [],
+    idUser: '1',
+  	modeConservation: ''
   },
-  created () {
-    var vm = this
-    axios.get('http://localhost:8080/myappWeb/services/rest/stock/1')
-      .then(function (response) {
-        vm.stocks = response.data
-      })
-  },
-  methods: {
-	    moment: function (date) {
-	      return moment(date);
-	    },
-	    date: function (date) {
-	      return moment(date).format('MMMM Do YYYY, h:mm:ss a');
-	    }
-	  },
-  filters: {
-	    moment: function (date) {
-	      return moment(date).format('MMMM Do YYYY, h:mm:ss a');
-	    }
+  watch: {
+	  modeConservation: function (mode, oldMode) {
+		  console.log('watcher !!');
+		  this.chargerStockModeConservation(mode)
 	  }
+  },
+  created: function () {
+	  console.log('Init');
+	  this.chargerStock()
+	  
+  },
+  methods: 
+  {
+	  produitSouris()
+	  {
+		  console.log('mouseOver Ok !!');
+	  },
+	  chargerStock()
+	  {
+		  id = document.getElementById('idUtilisateur').value;
+		  console.log('Stock.parUtilisateur: '+id);
+		  var vm = this
+	      axios.get('http://localhost:8080/myappWeb/services/rest/stock/'+id)
+	          .then(function (response) {
+	        	  vm.stocks = response.data
+	      })
+	  },
+		
+	  chargerStockModeConservation(mode)
+	  {
+		  id = document.getElementById('idUtilisateur').value;
+		  console.log('Stock.parModeConservation: id='+id+', modeConservation='+mode);
+		  var vm = this
+		  if(mode == "")
+		  {
+			  axios.get('http://localhost:8080/myappWeb/services/rest/stock?id='+id)
+			  	.then(function (response) {
+			  		vm.stocks = response.data
+				      })
+		  }
+		  else
+		  {
+			  axios.get('http://localhost:8080/myappWeb/services/rest/stock?id='+id+'&mode='+mode)
+				.then(function (response) {
+					vm.stocks = response.data
+				 })
+		  }
+	  },
+		
+	  moment: function (date) 
+	  {
+	      return moment(date);
+	  },
+	    
+	  date: function (date) 
+	  {
+	      return moment(date).format('MMMM Do YYYY, h:mm:ss a');
+	  }
+  },
+  filters: {
+	  moment: function (date) 
+	  {
+	    return moment(date).format('MMMM Do YYYY, h:mm:ss a');
+	  }
+  }
+  
 })
