960b3482672619c93aa0bad6e81ffa711ae4a9fa
wingy <liu.wingy@gmail.com>
2018-08-21 12:23:28 +0200

    interface IServiceFoodfriend

---
+++ myappCore/src/main/java/fr/afcepf/ai103/service/IServiceFoodfriend.java
@@ -0,0 +1,47 @@
+package fr.afcepf.ai103.service;
+
+import java.util.List;
+
+import fr.afcepf.ai103.data.Annulation;
+import fr.afcepf.ai103.data.Foodfriend;
+
+
+
+public interface IServiceFoodfriend {
+	
+	
+	// _________________METHODE DE TEST ___________________________________________________________
+	// Méthode de test
+	public Foodfriend obtenirRelationFoodfriend(int num);
+	
+	
+	// Méthode de test
+	public List<Foodfriend> rechercherTousLesFoodfriend();
+	
+	
+	// _________________METHODE D'AFFICHAGE ___________________________________________________________
+	// Obtenir la liste des foodfriend d'un utilisateur
+	public List<Foodfriend> rechercherMesFoodfriend(int numUtilisateur);
+	
+	
+	// Obtenir la liste de mes demandes de foodfriend recues
+	public List<Foodfriend> rechercherMesDemandesFoodfriendRecues(int numUtilisateur);
+	
+	// Obtenir la liste des demandes de foodfriend envoyees
+	public List<Foodfriend> rechercherMesDemandesFFEnvoyees(int numUtilisateur);
+
+	
+	// _______________METHODE D'INSERTION _____________________________________________________________
+	// Accepter une demande de foodfriend
+	public Foodfriend saveOrUpdate(Foodfriend ff);
+	
+
+	// Envoyer une demande de foodfriend
+	public Foodfriend save(Foodfriend ff);
+	
+
+
+
+	
+
+}
--- myappCore/src/main/java/fr/afcepf/ai103/service/ServiceFoodfriend.java
+++ myappCore/src/main/java/fr/afcepf/ai103/service/ServiceFoodfriend.java
@@ -8,12 +8,13 @@ import javax.ejb.Local;
 import javax.ejb.Stateless;
 
 import fr.afcepf.ai103.dao.IDaoFoodfriend;
+import fr.afcepf.ai103.data.Evaluation;
 import fr.afcepf.ai103.data.Foodfriend;
 import fr.afcepf.ai103.data.Utilisateur;
 
 @Stateless
 @Local
-public class ServiceFoodfriend {
+public class ServiceFoodfriend implements IServiceFoodfriend {
 	
 	private static final Date date_null = null;
 	@EJB
@@ -21,11 +22,13 @@ public class ServiceFoodfriend {
 	
 	// _________________METHODE DE TEST ___________________________________________________________
 	// Méthode de test
+	@Override
 	public Foodfriend obtenirRelationFoodfriend(int num) {
 		return daoff.rechercherFoodFriendParId(num);
 	}
 	
 	// Méthode de test
+	@Override
 	public List<Foodfriend> rechercherTousLesFoodfriend() {
 		System.out.println("rechercher tous les food friend SERVICE");
 		return daoff.rechercherTousLesFoodfriend();
@@ -33,17 +36,20 @@ public class ServiceFoodfriend {
 	
 	// _________________METHODE D'AFFICHAGE ___________________________________________________________
 	// Obtenir la liste des foodfriend d'un utilisateur
+	@Override
 	public List<Foodfriend> rechercherMesFoodfriend(int numUtilisateur) {
 		System.out.println("affichage utilisateur SERVICE" + numUtilisateur);
 		return daoff.obtenirlisteFoodfriend(numUtilisateur);
 	}
 	
 	// Obtenir la liste de mes demandes de foodfriend recues
+	@Override
 	public List<Foodfriend> rechercherMesDemandesFoodfriendRecues(int numUtilisateur) {
 		return daoff.obtenirlisteDemandesDeFoodfriendRecues(numUtilisateur);
 	}
 	
 	// Obtenir la liste des demandes de foodfriend envoyees
+	@Override
 	public List<Foodfriend> rechercherMesDemandesFFEnvoyees(int numUtilisateur) {
 		return daoff.obtenirlisteFoodfriendEnvoyees(numUtilisateur);
 	}
@@ -52,17 +58,20 @@ public class ServiceFoodfriend {
 	
 	// _______________METHODE D'INSERTION _____________________________________________________________
 	// Accepter une demande de foodfriend
+	@Override
 	public Foodfriend saveOrUpdate(Foodfriend ff) {
 		daoff.majRelationFoodfriend(ff);		
 		return ff;
 	}
 
 	// Envoyer une demande de foodfriend
+	@Override
 	public Foodfriend save(Foodfriend ff) {
 		ff = daoff.insererNouveauFoodfriend(ff);
 		return ff;
 	}
 
+	
 	
 	
 }
